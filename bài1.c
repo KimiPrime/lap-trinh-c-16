67c3055ed0e0e7167cdaa64d24b6a5adf819b242
KimiPrime <kingsan049@gmail.com>
2024-12-16 14:47:58 +0700

    Add files via upload

--- bài1.c
+++ bài1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    int x = 10;
-    int *ptr = &x;
-
-    printf("Gia tri cua x: %d\n", *ptr);
-    printf("Dia chi cua x: %p\n", ptr);
-
-    return 0;
+int main(){
+	int x=10;
+	int *ptr=&x;
+	printf("cach 1 bien \n");
+	printf("gia tri %d vi tri %d\n",x,&x);
+	printf("cach 2 con tro \n");
+	printf("gia tri %d vi tri %d",*ptr,ptr);
+	return 0;
 }
